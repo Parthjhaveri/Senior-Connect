c27e6b67cd0a6041bbdee50fd8736c3f3ba7cc98
Rumthorp <mattmerwinjennings@gmail.com>
2017-01-22 14:55:31 -0500

    updated api

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,33 @@
 const router = require("express").Router();
 const path = require("path")
 
+router.route('./api/createUser')
+.get((req, res) => {
+  User.Create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    zip: req.body.zip,
+    pantryLocation: req.body.pantryLocation,
+    location: req.body.location,
+    birthDate: req.body.birthDate,
+    password: req.body.password,
+    isAvailable: req.body.isAvailable
+  }).then(() => {
+    res.sendStatus(200)
+  }).catch((err) => {
+    console.log(err)
+    res.sendStatus(500)
+  })
+})
+
+router.route('./api/getAllUsers')
+.get((req, res) => {
+  User.findAll().then((data) => {
+    res.send(data)
+  }).catch((err) => {
+    console.log(err)
+    res.sendStatus(500)
+  })
+})
+
 module.exports = router
