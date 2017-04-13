480feab3bb817dc246e429398e8c560f4930da5d
Parthjhaveri <parthjhaveri209@gmail.com>
2017-04-13 17:42:27 -0400

    heroku deploy

--- server.js
+++ server.js
@@ -19,4 +19,5 @@ app.use(express.static('./bundle'));
 app.use('/', require('./routes'))
 
 
-app.listen(3000);
+// app.listen(3000);
+app.listen(process.env.PORT || '3000', () => console.log('Listening on port 3000'));
